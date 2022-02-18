df45ffd6bcbe0c3113158e4e299602934b419b5b
Adrijana <adrijana.jovicic@gmail.com>
2022-02-18 13:18:26 +0100

    Redux Setup & Auth Slice

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,20 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+const initialState = {
+    user: null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: '',
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {},
+    extraReducers: (buider) => {
+
+    }
+})
+
+export default authSlice.reducer
