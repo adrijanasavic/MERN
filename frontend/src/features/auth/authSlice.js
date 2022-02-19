f0f327be2d61fef3a78f6b4b871bdb427ceec098
Adrijana <adrijana.jovicic@gmail.com>
2022-02-19 11:02:35 +0100

    User Login

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -15,9 +15,21 @@ const initialState = {
 export const register = createAsyncThunk(
     'auth/register',
     async (user, thunkAPI) => {
-    console.log(user);
+        try {
+            return await authService.register(user)
+        } catch (error) {
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                error.message ||
+                error.toString()
+
+            return thunkAPI.rejectWithValue(message)
+        }
     }
 )
+
 // Login user
 export const login = createAsyncThunk(
     'auth/login',
@@ -69,6 +81,20 @@ export const authSlice = createSlice({
                 state.user = null
             })
             .addCase(login.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.user = action.payload
+            })
+            .addCase(login.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+                state.user = null
+            })
+            .addCase(logout.fulfilled, (state) => {
                 state.user = null
             })
     },
